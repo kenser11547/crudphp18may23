e6ea83e435a09d27f226d97f5503bad36796d016
kenser <koffy140221@gmail.com>
2023-05-18 11:31:50 -0600

    CAMBIO8

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 
 /*require './modelos/conexion.php';
 $resultado = conexion::ejecutar("INSERT INTO productos(producto_nombre, producto_precio) values('prueba','5')");
-var_dump($resultado);*/
+var_dump($resultado);
 
 
 require './modelos/conexion.php';
 $resultado = conexion::servir("SELECT * FROM productos");
 echo "<pre>";
 var_dump($resultado);
-echo "</pre>";
+echo "</pre>";*/
