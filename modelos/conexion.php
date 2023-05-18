24a680464b6bfca027973ca64b32e5f085d6fffe
kenser <koffy140221@gmail.com>
2023-05-18 10:29:18 -0600

    cambios3

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -6,6 +6,7 @@ abstract class  Conexion{
     private static function conectar(){
         try{
             self::$conexion = new PDO("informix:host=host.docker.internal; service=9088;database=mdn; server=informix; protocol=onsoctcp;EnableScrollableCursors=1", "informix", "in4mix");
+            echo "conectado";
         }catch(PDOException $e){
             echo "ERROR DE CONEXION DE BD";
             exit;
