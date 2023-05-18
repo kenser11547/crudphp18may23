6b3d6b4ead9d2829f34f0af000aa4f95652de402
kenser <koffy140221@gmail.com>
2023-05-18 12:28:32 -0600

    CAMBIOSS

--- controladores/productos/guardar.php
+++ controladores/productos/guardar.php
@@ -1,3 +1,11 @@
 <?php
 
-echo 'estas en la carpeta guardar';
+require '../../modelos/productos.php';
+
+try{
+    $producto = new Producto($_POST);
+    $resultado = $producto->guardar();
+    var_dump($resultado);
+}catch (PDOException $e){
+    echo $e->getMessage();
+}
--- vistas/productos/index.php
+++ vistas/productos/index.php
@@ -11,7 +11,7 @@
     <div class="container">
         <h1 class="text-center">Formulario de productos</h1>
         <div class="row justify-content-center">
-            <form action="./CRUDPHP18MAY23/controladores/productos/guardar.php" method="POST" class="col-lg-8 border bg-light p-3">
+            <form action="/crudphp18MAY23/controladores/productos/guardar.php" method="POST" class="col-lg-8 border bg-light p-3">
                 <div class="row mb-3">
                     <div class="col">
                         <label for="producto_nombre">Nombre del producto</label>
