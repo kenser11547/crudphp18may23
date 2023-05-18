14290ed5b8a28fcab71c196b1c5057706e503d1d
kenser <koffy140221@gmail.com>
2023-05-18 12:28:25 -0600

    CAMBIOFINALES

---
+++ modelos/productos.php
@@ -0,0 +1,25 @@
+<?php
+require 'Conexion.php';
+
+class Producto extends Conexion{
+    public $producto_id;
+    public $producto_nombre;
+    public $producto_precio;
+    public $producto_situacion;
+
+    public function __construct($args = [] ){
+        $this->producto_id = $args['producto_id'] ?? null;
+        $this->producto_nombre = $args['producto_nombre'] ?? '';
+        $this->producto_precio = $args['producto_precio'] ?? '';
+        $this->producto_situacion = $args['producto_situacion'] ?? '';
+        
+
+    }
+
+    public function guardar(){
+        $sql = "INSERT INTO productos(producto_nombre, producto_precio) values ()"
+    }
+
+
+
+}
